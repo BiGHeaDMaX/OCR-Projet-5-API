ce79ecc43e8f44a64b171b402123043f5789ac96
bigheadmax <bigheadmax@hotmail.com>
2023-11-03 09:48:24 +0100

    test all 0

--- main.py
+++ main.py
@@ -18,4 +18,7 @@ async def prediction_form():
     # Lancement de l'app sur un serveur uvicorn
 #    uvicorn.run(app, host="127.0.0.1", port=8000)
 
-uvicorn.run(app, host="127.0.0.1", port=8000)
+#uvicorn.run(app, host="127.0.0.1", port=8000)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000)
