e59442a5e14cc0f68bb84782b3622dfb1e53e236
bigheadmax <bigheadmax@hotmail.com>
2023-11-03 09:32:23 +0100

    Hello world

---
+++ main.py
@@ -0,0 +1,21 @@
+# Imports spécifiques à l'API
+from fastapi import FastAPI
+import uvicorn
+
+# Instanciation de notre API
+app = FastAPI()
+
+##################
+# Page d'accueil #
+##################
+@app.get("/")
+async def prediction_form():
+    return "Bonjour"
+
+
+#if __name__ == "__main__":
+    #import uvicorn
+    # Lancement de l'app sur un serveur uvicorn
+#    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+uvicorn.run(app, host="127.0.0.1", port=8000)
