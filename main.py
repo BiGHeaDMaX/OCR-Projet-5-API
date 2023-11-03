154e3c423e34351501edf0d497ec527229328ddc
bigheadmax <bigheadmax@hotmail.com>
2023-11-03 10:27:22 +0100

    suppr run uvicorn

--- main.py
+++ main.py
@@ -20,5 +20,5 @@ async def prediction_form():
 
 #uvicorn.run(app, host="127.0.0.1", port=8000)
 
-if __name__ == '__main__':
-    uvicorn.run('main:app', host='0.0.0.0', port=8000)
+#if __name__ == '__main__':
+#    uvicorn.run('main:app', host='0.0.0.0', port=8000)
