3a2a1d2a80063188a2e35590965e971cd7cefc61
bigheadmax <bigheadmax@hotmail.com>
2023-11-06 14:18:47 +0100

    remove async et clean

--- app/main.py
+++ app/main.py
@@ -9,16 +9,8 @@ app = FastAPI()
 # Page d'accueil #
 ##################
 @app.get("/")
-async def prediction_form():
+def prediction_form():
     return "Bonjour new 2"
-
-
-#if __name__ == "__main__":
-    #import uvicorn
-    # Lancement de l'app sur un serveur uvicorn
-#    uvicorn.run(app, host="127.0.0.1", port=8000)
-
-#uvicorn.run(app, host="127.0.0.1", port=8000)
 
 if __name__ == '__main__':
     uvicorn.run('main:app')
