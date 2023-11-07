bc47a2d9aaae46f5e584cc0cc142efe452c10e5c
bigheadmax <bigheadmax@hotmail.com>
2023-11-07 21:04:14 +0100

    test fail test

--- app/main.py
+++ app/main.py
@@ -35,7 +35,7 @@ def array_converter(document):
     doc = document.replace('[', '').replace(']', '')
     doc = doc.split()
     doc = np.array([float(item) for item in doc])
-    return doc
+    return "coucou"#doc
 
 ##########################
 # Fonction de prédiction #
